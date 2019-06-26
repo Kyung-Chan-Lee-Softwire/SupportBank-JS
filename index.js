c0f590505a4725ba899da9ac6ee0101b32e65852
Kyung Lee <Kyung.Lee@softwire.com>
2019-06-26 10:16:47 +0100

    Added Logging

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const filePath1 = path.join(__dirname,'./Transactions2014.csv');
+const filePath2 = path.join(__dirname,'./DodgyTransactions2015.csv');
+const moment = require('moment');
+const readlineSync = require('readline-sync');
+var TransactionData = [];
+var Accounts = [];
+var Save1 = fs.readFileSync(filePath1, {encoding: 'utf-8'});
+var Save2 = fs.readFileSync(filePath2, {encoding: 'utf-8'});
+var FileLine1 = Save1.split("\n");
+var FileLine2 = Save2.split("\n");
+var log4js = require('log4js')
+var NameLog = [];
+
+log4js.configure({
+    appenders: {
+        file: { type: 'fileSync', filename: 'logs/debug.log' }
+    },
+    categories: {
+        default: { appenders: ['file'], level: 'info'}
+    }
+});
+
 class Transaction
 {
     constructor(TrDate, From, To, Narrative, Amount)
@@ -55,34 +79,65 @@ class Account
     }
 }
 
-const fs = require('fs');
-const path = require('path');
-const filePath1 = path.join(__dirname,'./Transactions2014.csv');
-const filePath2 = path.join(__dirname,'./DodgyTransactions2015.csv');
-const moment = require('moment');
-const readlineSync = require('readline-sync');
-var TransactionData = [];
-var Accounts = [];
-var Save1 = fs.readFileSync(filePath1, {encoding: 'utf-8'});
-var Save2 = fs.readFileSync(filePath2, {encoding: 'utf-8'});
-var FileLine1 = Save1.split("\n");
-var FileLine2 = Save2.split("\n");
 
-for(i=0;i<FileLine1.length;i++)
+var logger = log4js.getLogger('Transactions2014.csv');
+
+logger.info('Starts reading from Transactions2014.csv');
+
+for(i=1;i<FileLine1.length;i++)
 {
     let tmp = FileLine1[i].split(',');
-    if(isNaN(tmp[4])) continue;
+    if(tmp.length != 5)
+    {
+        logger.error('Line ' + (i-1) + ' has invalid format');
+        console.log('Line '+ (i-1) + ' of Transactions2014.csv is not processed. See Log for details');
+        continue;
+    }
+    if(!moment(tmp[0],'DD-MM-YYYY').isValid())
+    {
+        logger.error('Line ' + (i-1) + ' has invalid date');
+        console.log('Line '+ (i-1) + ' of Transactions2014.csv is not processed. See Log for details');
+        continue;
+    }
+    if(isNaN(tmp[4]))
+    {
+        logger.error('Line ' + (i-1) + ' has invalid amount of money');
+        console.log('Line '+ (i-1) + ' of Transactions2014.csv is not processed. See Log for details');
+        continue;
+    }
     TransactionData .push(new Transaction(moment(tmp[0],'DD-MM-YYYY'),tmp[1],tmp[2],tmp[3],Number(tmp[4])));
 }
-for(i=0;i<FileLine2.length;i++)
+
+logger = log4js.getLogger('DodgyTransactions2015.csv');
+
+logger.info('Starts reading from DodgyTransactions2015.csv');
+
+for(i=1;i<FileLine2.length;i++)
 {
     let tmp = FileLine2[i].split(',');
-    if(isNaN(tmp[4])) continue;
+    if(tmp.length != 5)
+    {
+        logger.error('Line ' + (i-1) + ' has invalid format');
+        console.log('Line '+ (i-1) + ' of DodgyTransactions2015.csv is not processed. See Log for details');
+        continue;
+    }
+    if(!moment(tmp[0],'DD-MM-YYYY').isValid())
+    {
+        logger.error('Line ' + (i-1) + ' has invalid date');
+        console.log('Line '+ (i-1) + ' of DodgyTransactions2015.csv is not processed. See Log for details');
+        continue;
+    }
+    if(isNaN(tmp[4]))
+    {
+        logger.error('Line ' + (i-1) + ' has invalid amount of money');
+        console.log('Line '+ (i-1) + ' of DodgyTransactions2015.csv is not processed. See Log for details');
+        continue;
+    }
     TransactionData .push(new Transaction(moment(tmp[0],'DD-MM-YYYY'),tmp[1],tmp[2],tmp[3],Number(tmp[4])));
 }
 
 console.log('Welcome to SupportBank');
-NameLog = [];
+
 for( i = 0; i < TransactionData.length; i++)
 {
     if(!NameLog.includes(TransactionData[i].From))
